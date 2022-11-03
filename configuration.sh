9754c477ccdb17f4b87bdf27143b609e86dc389c
NickBorgers <nborgers@pinterest.com>
2022-11-03 19:29:35 +0000

    detect presence/absence of osqueryi

--- configuration.sh
+++ configuration.sh
@@ -1,3 +1,9 @@
+if ! command -v osqueryi &> /dev/null
+then
+    echo "Must have osqueryi available, please install it and put it on your PATH"
+    exit
+fi
+
 VULNERABLE_OPENSSL_CONTAINER_IMAGE=clojure:temurin-17-lein-2.9.8-jammy
 OSQUERY_QUERY=$(cat <<EOF
 WITH FIRST_QUERY AS (SELECT DISTINCT
--- demonstrate_running_osquery_in_container_successfully.sh
+++ demonstrate_running_osquery_in_container_successfully.sh
@@ -7,7 +7,7 @@ source ${BASH_SOURCE%/*}/configuration.sh
 docker stop vulnerable-openssl || true
 # Start OpenSSL with long-running task (speed test)
 docker run --rm --detach --name vulnerable-openssl \
-  --mount type=bind,source=/usr/bin/osqueryi,destination=/usr/bin/osqueryi \
+  --mount type=bind,source=$(command -v osqueryi),destination=/usr/bin/osqueryi \
   ${VULNERABLE_OPENSSL_CONTAINER_IMAGE} openssl speed
 # Sleep long enough for OpenSSL to start its work
 sleep 2
